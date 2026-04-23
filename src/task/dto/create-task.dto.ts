41b4dcc6bd38708dc4a3cda1362df960560f3caa
igor.kudlo <igoreklida@gmail.com>
2026-04-23 14:21:40 +0300

    validation in DTO

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,3 +1,37 @@
+import {
+  IsArray,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Length,
+} from 'class-validator';
+
+export enum TaskTag {
+  WORK = 'work',
+  STUDY = 'study',
+  HOME = 'home',
+}
+
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 40)
   title: string;
+
+  @IsString({ message: 'Description must be a string' })
+  @IsOptional()
+  description: string;
+
+  @IsInt({ message: 'Priority must be an integer' })
+  @IsPositive({ message: 'Priority must be a positive number' })
+  @IsOptional()
+  priority: number;
+
+  @IsArray({ message: 'Tags must be an array' })
+  @IsEnum(TaskTag, { each: true, message: 'Tag must be a valid tag' })
+  @IsOptional()
+  tags: TaskTag[];
 }
--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,4 +1,11 @@
+import { IsBoolean, IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class UpdateTaskDto {
+  @IsString({ message: 'Title must be a string' })
+  @IsNotEmpty({ message: 'Title is required' })
+  @Length(2, 40, { message: 'Title must be between 2 and 40 characters' })
   title: string;
+
+  @IsBoolean({ message: 'Status must be a boolean' })
   isCompleted: boolean;
 }
